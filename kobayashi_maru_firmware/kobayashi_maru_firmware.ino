0206529c2550bf1bc390e95652a002103e22657f
Justin Eskesen <justin.eskesen@gmail.com>
2018-01-11 23:00:00 +0000

    firmware will publish PWM servo outputs as ros UInt16 msgs

--- kobayashi_maru_firmware/kobayashi_maru_firmware.ino
+++ kobayashi_maru_firmware/kobayashi_maru_firmware.ino
@@ -1,22 +1,35 @@
 #include "Arduino.h"
 #include "PinDefs.h"
 
+#include <ros.h>
+ros::NodeHandle  nh;
+
+#include <std_msgs/UInt16.h>
+std_msgs::UInt16 steering_pwm_msg, throttle_pwm_msg;
+ros::Publisher steering_pwm("steering_pwm", &steering_pwm_msg);
+ros::Publisher throttle_pwm("throttle_pwm", &throttle_pwm_msg);
+
 #include <Servo.h>
-Servo steering, throttle;
+Servo steering_servo, throttle_servo;
 
 #include "RCInput.h"
 RCInput rc;
 
+#include "ros.h"
+
 void setup()
 {
-	Serial.begin(115200);
+	nh.initNode();
+  nh.advertise(steering_pwm);
+  nh.advertise(throttle_pwm);
+  
 	rc.init(CPPM_PIN);
 
-	throttle.attach(THROTTLE_ESC_PIN, 1000, 2000);
-	throttle.writeMicroseconds(DEFAULT_PULSE_WIDTH);
+	throttle_servo.attach(THROTTLE_ESC_PIN, 1000, 2000);
+	throttle_servo.writeMicroseconds(DEFAULT_PULSE_WIDTH);
 
-	steering.attach(STEERING_SERVO_PIN, 1000, 2000);
-	steering.writeMicroseconds(DEFAULT_PULSE_WIDTH);
+	steering_servo.attach(STEERING_SERVO_PIN, 1000, 2000);
+	steering_servo.writeMicroseconds(DEFAULT_PULSE_WIDTH);
 
 }
 
@@ -26,19 +39,27 @@ void loop()
 	{
 		if (rc.isManual())
 		{
-			steering.writeMicroseconds((int) rc.getSteering());
-			throttle.writeMicroseconds((int) rc.getThrottle());
+			steering_pwm_msg.data = (uint16_t)rc.getSteering();
+			throttle_pwm_msg.data = (uint16_t)rc.getThrottle();
 		}
 		else
 		{
-			steering.writeMicroseconds(DEFAULT_PULSE_WIDTH);
-			throttle.writeMicroseconds(DEFAULT_PULSE_WIDTH);
+			steering_pwm_msg.data = (uint16_t)DEFAULT_PULSE_WIDTH;
+			throttle_pwm_msg.data = (uint16_t)DEFAULT_PULSE_WIDTH;
 		}
 	}
 	else
 	{
-		steering.writeMicroseconds(DEFAULT_PULSE_WIDTH);
-		throttle.writeMicroseconds(DEFAULT_PULSE_WIDTH);
+		steering_pwm_msg.data = (uint16_t)DEFAULT_PULSE_WIDTH;
+		throttle_pwm_msg.data = (uint16_t)DEFAULT_PULSE_WIDTH;
 	}
+ 
+	steering_servo.writeMicroseconds(steering_pwm_msg.data);
+	throttle_servo.writeMicroseconds(throttle_pwm_msg.data);
+	steering_pwm.publish(&steering_pwm_msg);
+	throttle_pwm.publish(&throttle_pwm_msg);
+
+	nh.spinOnce();
+
 }
 
