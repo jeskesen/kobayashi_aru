a62f20d857f5a61523abc9975c561bb524e79f5e
Justin Eskesen <justin.eskesen@gmail.com>
2018-08-07 10:09:48 -0700

    reorganized controller code into a class.  publish joint_state messages as control outputs

--- kobayashi_maru_control/src/follow_path.cpp
+++ kobayashi_maru_control/src/follow_path.cpp
@@ -1,27 +1,90 @@
 #include <ros/ros.h>
 #include <tf/transform_listener.h>
+#include <sensor_msgs/JointState.h>
 
+class PathFollower
+{
+public:
+  PathFollower();
 
-int main(int argc, char** argv){
-  ros::init(argc, argv, "follow_path");
-
+  void spin();
+protected:
+  void init();
+  void spinOnce();
   ros::NodeHandle node;
   tf::TransformListener listener;
+  tf::StampedTransform transform;
 
-  ros::Rate rate(10.0);
-  while (node.ok()){
-    tf::StampedTransform transform;
-    try{
+  ros::Publisher rightSteeringPub, leftSteeringPub, tireVelPub;
+  sensor_msgs::JointState rightSteeringMsg, leftSteeringMsg, tireVelMsg;
+  ros::Rate rate;
+};
+
+PathFollower::PathFollower() :
+    rate(10.0)
+{
+}
+
+void PathFollower::init()
+{
+  rightSteeringPub = node.advertise<sensor_msgs::JointState>("right_steering_joint", 1000);
+  leftSteeringPub = node.advertise<sensor_msgs::JointState>("left_steering_joint", 1000);
+  tireVelPub = node.advertise<sensor_msgs::JointState>("tire_rotation", 1000);
+
+
+  rightSteeringMsg.position.push_back(0.0);
+  rightSteeringMsg.name.push_back("right_steering_joint");
+  leftSteeringMsg.position.push_back(0.0);
+  leftSteeringMsg.name.push_back("left_steering_joint");
+  tireVelMsg.velocity.push_back(0.0); // so that it's allocated
+}
+
+void PathFollower::spin()
+{
+  ros::Duration(1.0).sleep();
+
+  init();
+
+  while (node.ok())
+  {
+    try
+    {
       listener.lookupTransform("odom", "base_link", ros::Time(0), transform);
-    }
-    catch (tf::TransformException ex){
-      ROS_ERROR("%s",ex.what());
+    } catch (tf::TransformException ex)
+    {
+      ROS_ERROR("%s", ex.what());
       ros::Duration(1.0).sleep();
     }
 
-    ROS_INFO("%f %f %f", transform.getOrigin().getX(), transform.getOrigin().getY(), transform.getOrigin().getZ());
+    spinOnce();
+
     rate.sleep();
   }
+}
+
+void PathFollower::spinOnce()
+{
+
+  tireVelMsg.velocity[0]=1.0;
+  rightSteeringMsg.position[0]=1.0;
+  leftSteeringMsg.position[0]=1.0;
+
+  tireVelPub.publish(tireVelMsg);
+  rightSteeringPub.publish(rightSteeringMsg);
+  leftSteeringPub.publish(leftSteeringMsg);
+
+  ROS_INFO("%f %f %f",
+      transform.getOrigin().getX(),
+      transform.getOrigin().getY(),
+      transform.getOrigin().getZ());
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "follow_path");
+
+  PathFollower follower;
+  follower.spin();
   return 0;
-};
+}
 
