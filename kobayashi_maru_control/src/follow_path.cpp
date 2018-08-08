fb99bbc01dc5c483837a76765dbacc7b1eceb7b1
Justin Eskesen <justin.eskesen@gmail.com>
2018-08-07 17:06:32 -0700

    controller now receives a path as input, and has the framework to iterate through path points, and control on them (though it doesn't YET do the control part)

--- kobayashi_maru_control/src/follow_path.cpp
+++ kobayashi_maru_control/src/follow_path.cpp
@@ -1,7 +1,7 @@
 #include <ros/ros.h>
 #include <tf/transform_listener.h>
 #include <sensor_msgs/JointState.h>
-
+#include <nav_msgs/Path.h>
 class PathFollower
 {
 public:
@@ -11,18 +11,32 @@ public:
 protected:
   void init();
   void spinOnce();
+  void onNewPathReceived(const nav_msgs::Path::ConstPtr& msg);
   ros::NodeHandle node;
   tf::TransformListener listener;
   tf::StampedTransform transform;
 
   ros::Publisher rightSteeringPub, leftSteeringPub, tireVelPub;
+  ros::Subscriber pathSubscriber;
+
   sensor_msgs::JointState rightSteeringMsg, leftSteeringMsg, tireVelMsg;
   ros::Rate rate;
+
+  /// this is the path we will follow
+  nav_msgs::Path path;
+  /// this holds our progress as we iterate through the path.
+  std::vector<geometry_msgs::PoseStamped>::iterator currentPose;
+
+  double captureRadius;
+  double xVel;
 };
 
 PathFollower::PathFollower() :
     rate(10.0)
 {
+  currentPose=path.poses.end();
+  captureRadius = 1.0;
+  xVel = 1.0;
 }
 
 void PathFollower::init()
@@ -30,6 +44,7 @@ void PathFollower::init()
   rightSteeringPub = node.advertise<sensor_msgs::JointState>("right_steering_joint", 1000);
   leftSteeringPub = node.advertise<sensor_msgs::JointState>("left_steering_joint", 1000);
   tireVelPub = node.advertise<sensor_msgs::JointState>("tire_rotation", 1000);
+  pathSubscriber = node.subscribe("path_to_follow", 1000, &PathFollower::onNewPathReceived, this);
 
 
   rightSteeringMsg.position.push_back(0.0);
@@ -58,25 +73,51 @@ void PathFollower::spin()
 
     spinOnce();
 
+    tireVelPub.publish(tireVelMsg);
+    rightSteeringPub.publish(rightSteeringMsg);
+    leftSteeringPub.publish(leftSteeringMsg);
+
+    ros::spinOnce();
     rate.sleep();
   }
 }
 
+
 void PathFollower::spinOnce()
 {
 
-  tireVelMsg.velocity[0]=1.0;
-  rightSteeringMsg.position[0]=1.0;
-  leftSteeringMsg.position[0]=1.0;
+  // are we done? if so, stop.
+  if(currentPose==path.poses.end())
+  {
+    tireVelMsg.velocity[0]=0.0;
+    rightSteeringMsg.position[0]=0.0;
+    leftSteeringMsg.position[0]=0.0;
+    return;
+  }
 
-  tireVelPub.publish(tireVelMsg);
-  rightSteeringPub.publish(rightSteeringMsg);
-  leftSteeringPub.publish(leftSteeringMsg);
+  // are we at our current goal point?  if so, increment our goal iterator, and recurse.
+  tf::Vector3 currentGoalPosition(currentPose->pose.position.x, currentPose->pose.position.y, 0.0);
+  ROS_INFO("Distance %f", currentGoalPosition.distance(transform.getOrigin()));
 
-  ROS_INFO("%f %f %f",
-      transform.getOrigin().getX(),
-      transform.getOrigin().getY(),
-      transform.getOrigin().getZ());
+  if(currentGoalPosition.distance(transform.getOrigin()) < captureRadius)
+  {
+    currentPose++;
+    return spinOnce();
+  }
+
+  tireVelMsg.velocity[0]=xVel/ 0.14605;
+  rightSteeringMsg.position[0]=0.0;
+  leftSteeringMsg.position[0]=0.0;
+
+
+
+}
+
+void PathFollower::onNewPathReceived(const nav_msgs::Path::ConstPtr& msg)
+{
+  path = *(msg.get());
+  currentPose = path.poses.begin();
+
 }
 
 int main(int argc, char** argv)
